4d6242653561665babf2d7a2d6f0066633428b8d
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-08 01:56:32 +0300

    Add PortfolioSection component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
 import ProductsSection from '../components/ProductsSection';
+import PortfolioSection from '../components/PortfolioSection';
 
 export default function HomePage() {
   return (
@@ -15,6 +16,7 @@ export default function HomePage() {
     <AboutSection />
     <ServicesSection />
     <ProductsSection />
+    <PortfolioSection />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
